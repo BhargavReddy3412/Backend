c57d2ae8e273e37a5136c1e0bf32d2f618e381f5
Bhargav Reddy <bhargavreddyp948@gmail.com>
2025-06-18 15:19:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
